fb65aeafff8cecde9618ca48ad1ce01dcdf79817
Nimmy The Great <Nimrod@reshef.com>
2020-08-04 18:54:29 +0300

    service finished! :)

--- src/main/java/com/enosh/jwtauth/services/JwtService.java
+++ src/main/java/com/enosh/jwtauth/services/JwtService.java
@@ -1,15 +1,19 @@
 package com.enosh.jwtauth.services;
 
 import com.enosh.jwtauth.model.UserEntity;
+import io.jsonwebtoken.Claims;
 import io.jsonwebtoken.Jwts;
 import io.jsonwebtoken.SignatureAlgorithm;
 import org.springframework.beans.factory.annotation.Value;
+import org.springframework.security.core.userdetails.UserDetails;
 import org.springframework.stereotype.Service;
 
 import javax.crypto.spec.SecretKeySpec;
 import javax.xml.bind.DatatypeConverter;
 import java.security.Key;
 import java.util.Date;
+import java.util.Map;
+import java.util.function.Function;
 
 import static io.jsonwebtoken.SignatureAlgorithm.*;
 import static javax.xml.bind.DatatypeConverter.*;
@@ -20,14 +24,28 @@ public class JwtService {
     @Value("${secret}")
     private String SECRET_KEY;
 
+    public Function<Claims, String> extractSubject = Claims::getSubject;
+
     public <T extends UserEntity> String encodeJwt(T subject) {
         byte[] secretBytes = parseBase64Binary(SECRET_KEY);
         Key key = new SecretKeySpec(secretBytes, HS256.getJcaName());
+
         return Jwts.builder()
                 .setIssuedAt(new Date(System.currentTimeMillis()))
                 .setId(String.valueOf(subject.getId()))
                 .setSubject(subject.getEmail())
                 .signWith(HS256, key)
                 .compact();
+    }
+
+    public Claims decodeJwt(String jwt) {
+        return Jwts.parser()
+                .setSigningKey(parseBase64Binary(SECRET_KEY))
+                .parseClaimsJws(jwt)
+                .getBody();
+    }
+
+    public boolean validateToken(Claims claims, UserDetails userDetails) {
+        return extractSubject.apply(claims).equals(userDetails.getUsername());
     }
 }
--- src/test/java/com/enosh/jwtauth/JwtauthApplicationTests.java
+++ src/test/java/com/enosh/jwtauth/JwtauthApplicationTests.java
@@ -1,5 +1,6 @@
 package com.enosh.jwtauth;
 
+import com.enosh.jwtauth.model.Company;
 import com.enosh.jwtauth.services.JwtService;
 import org.junit.jupiter.api.Test;
 import org.springframework.beans.factory.annotation.Autowired;
@@ -13,6 +14,25 @@ class JwtauthApplicationTests {
 
     @Test
     void contextLoads() {
+    }
+
+    @Test
+    void encode(){
+        Company company = new Company(
+                "aroma@cool",
+                "cool-aroma",
+                "123"
+        );
+        company.setId(1L);
+
+        String jwt = service.encodeJwt(company);
+        System.out.println(jwt);
+    }
+
+    @Test
+    void decode(){
+        String jwt = "eyJhbGciOiJIUzI1NiJ9.eyJpYXQiOjE1OTY1NTU4NzcsImp0aSI6IjEiLCJzdWIiOiJhcm9tYUBjb29sIn0.L8xVLcWQ2ffLr2sSCgWtVok7FLsI4_yeUXhLd63Mpug";
+        System.out.println(service.decodeJwt(jwt));
     }
 
 }
