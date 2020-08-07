700238a4916f3503a5b93ea22b83f39147ecf58b
Nimmy The Great <Nimrod@reshef.com>
2020-08-07 11:00:05 +0300

    admin

--- src/main/java/com/enosh/jwtauth/controller/AdminController.java
+++ src/main/java/com/enosh/jwtauth/controller/AdminController.java
@@ -1,8 +1,18 @@
 package com.enosh.jwtauth.controller;
 
+import com.enosh.jwtauth.model.LoginDto;
+import com.enosh.jwtauth.model.UserEntity;
+import com.enosh.jwtauth.services.AdminDetailsService;
 import com.enosh.jwtauth.services.JwtService;
 import lombok.AllArgsConstructor;
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
 import org.springframework.security.authentication.AuthenticationManager;
+import org.springframework.security.authentication.BadCredentialsException;
+import org.springframework.security.authentication.UsernamePasswordAuthenticationToken;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
@@ -11,6 +21,36 @@ import org.springframework.web.bind.annotation.RestController;
 @RequestMapping("/admin")
 public class AdminController {
 
+    @Value("${admin-username}")
+    private String username;
+
+    @Value("${admin-password}")
+    private String password;
+
     private final AuthenticationManager authenticationManager;
-    private JwtService jwtService;
+    private final JwtService jwtService;
+
+    @PostMapping("/authenticate")
+    public ResponseEntity authenticate(@RequestBody LoginDto loginDto) {
+        try {
+            authenticationManager.authenticate(
+                    new UsernamePasswordAuthenticationToken(
+                            loginDto.getEmail(),
+                            loginDto.getPassword()
+                    )
+            );
+
+        } catch (BadCredentialsException e) {
+            return ResponseEntity
+                    .status(HttpStatus.FORBIDDEN)
+                    .body(e.getMessage());
+        }
+
+        return ResponseEntity.ok(
+                jwtService.encodeAdmin(
+                        new UserEntity(username, password)
+                )
+        );
+    }
+
 }
