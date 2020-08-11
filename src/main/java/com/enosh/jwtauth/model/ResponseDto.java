37b4091a3b2d2637e3d78c3ef7112f24c69fe629
Nimmy The Great <Nimrod@reshef.com>
2020-08-11 20:27:36 +0300

    controller

--- src/main/java/com/enosh/jwtauth/model/ResponseDto.java
+++ src/main/java/com/enosh/jwtauth/model/ResponseDto.java
@@ -1,8 +1,14 @@
 package com.enosh.jwtauth.model;
 
 import lombok.AllArgsConstructor;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
 
+@NoArgsConstructor
 @AllArgsConstructor
+@Getter
+@Setter
 public class ResponseDto<T> {
 
     private boolean success;
